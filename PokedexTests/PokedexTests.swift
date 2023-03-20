23ea976b436367875ae6c27bdd586ae289c41f98
Valentin <harudareaper@gmail.com>
2023-03-20 11:13:12 -0700

    added pokeball picture

--- PokedexTests/PokedexTests.swift
+++ PokedexTests/PokedexTests.swift
@@ -10,7 +10,7 @@ import Combine
 @testable import Pokedex
 
 class PokedexTests: XCTestCase {
-
+    
     override func setUp() {
         
     }
@@ -22,42 +22,42 @@ class PokedexTests: XCTestCase {
     var subscriptions = Set<AnyCancellable>()
     
     func test_getting_pokemons_success(){
-                let result = Result<[Pokemon], APIError>.success([Pokemon.example()])
-               
-               let fetcher = PokemonFetcher(service: APIMockService(result: result))
-               
-               let promise = expectation(description: "getting pokemons")
-               
-               fetcher.$pokemons.sink { pokemons in
-                   if pokemons.count > 0 {
-                       promise.fulfill()
-                   }
-               }.store(in: &subscriptions)
-               
-              
-               wait(for: [promise], timeout: 2)
+        let result = Result<[Pokemon], APIError>.success([Pokemon.example()])
+        
+        let fetcher = PokemonFetcher(service: APIMockService(result: result))
+        
+        let promise = expectation(description: "getting pokemons")
+        
+        fetcher.$pokemons.sink { pokemons in
+            if pokemons.count > 0 {
+                promise.fulfill()
+            }
+        }.store(in: &subscriptions)
+        
+        
+        wait(for: [promise], timeout: 2)
     }
     
     func test_loading_error() {
-           
-             let result = Result<[Pokemon], APIError>.failure(APIError.badURL)
-             let fetcher = PokemonFetcher(service: APIMockService(result: result))
-             
-            let promise = expectation(description: "show error message")
-            fetcher.$pokemons.sink { pokemons in
-                if !pokemons.isEmpty {
-                    XCTFail()
-                }
-            }.store(in: &subscriptions)
-            
-            
-            fetcher.$errorMessage.sink { message in
-                if message != nil {
-                    promise.fulfill()
-                }
-            }.store(in: &subscriptions)
-            
-            wait(for: [promise], timeout: 2)
-            
-        }
+        
+        let result = Result<[Pokemon], APIError>.failure(APIError.badURL)
+        let fetcher = PokemonFetcher(service: APIMockService(result: result))
+        
+        let promise = expectation(description: "show error message")
+        fetcher.$pokemons.sink { pokemons in
+            if !pokemons.isEmpty {
+                XCTFail()
+            }
+        }.store(in: &subscriptions)
+        
+        
+        fetcher.$errorMessage.sink { message in
+            if message != nil {
+                promise.fulfill()
+            }
+        }.store(in: &subscriptions)
+        
+        wait(for: [promise], timeout: 2)
+        
+    }
 }
